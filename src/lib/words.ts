8c0f3e693ff90e2c886d3e6a3f8fb77a541f1676
Erin Crosland <erin.crosland@gmail.com>
2022-03-27 13:48:11 -0700

    `getWordOfDay` should return a "tomorrow" that corresponds to midnight of the following day.

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -1,6 +1,12 @@
 import { WORDS } from '../constants/wordlist'
 import { VALIDGUESSES } from '../constants/validGuesses'
 
+import dayjs from 'dayjs'
+import customParseFormat from 'dayjs/plugin/customParseFormat'
+
+dayjs.extend(customParseFormat)
+
+
 export const isWordInWordList = (word: string) => {
   return (
     WORDS.includes(word.toLowerCase()) ||
@@ -16,18 +22,17 @@ export const getLatinDefinition = (word: string) => {
   return `https://www.latindictionary.io/words/?word=${word}`
 }
 
-export const getWordOfDay = () => {
-  // January 1, 2022 Game Epoch
-  const epochMs = 1641013200000
-  const now = Date.now()
-  const msInDay = 86400000
-  const index = Math.floor((now - epochMs) / msInDay)
-  const nextday = (index + 1) * msInDay + epochMs
+const getWordOfDay = () => {
+  // January 1, 2022, at midnight local time, is the Game Epoch
+  const epoch   = dayjs('01-01-2022 00:00:00', 'MM-DD-YYYY HH:mm:ss')
+  const now     = dayjs()
+  const index   = now.diff(epoch, 'day')
+  const nextday = now.startOf('day').add(1, 'day')
 
   return {
-    solution: WORDS[index].toUpperCase(),
+    solution:      WORDS[index].toUpperCase(),
     solutionIndex: index,
-    tomorrow: nextday,
+    tomorrow:      nextday.valueOf(),
   }
 }
 
