ed7ae04600ed6ef94a12c13014eb44a31f92c783
Theodore Tarr <theodore@tarr.com>
2023-12-21 19:37:37 -0500

    latin congrats

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,6 +1,6 @@
 export const GAME_TITLE = process.env.REACT_APP_GAME_NAME!
 
-export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
+export const WIN_MESSAGES = ['Bene factum!', 'Optime!']
 export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
 export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
